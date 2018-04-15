6a91b2bf73cbebb74a8af89d6a99e575df7215b8
Si Parker <si@plogic.co.uk>
2018-04-15 16:40:17 +0100

    fixed: 2x declarative bases

--- pi_fly/go.py
+++ pi_fly/go.py
@@ -10,9 +10,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from model import Sensor
+from model import Sensor, Base
 
-Base = declarative_base()
 
 class AbstractSensorDecode(object):
     def __init__(self, *args, **kwargs):
