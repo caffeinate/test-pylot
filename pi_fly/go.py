3343386ec1cb6b4d2fbf5a8845c448538fe8b171
Si Parker <si@plogic.co.uk>
2018-11-16 11:09:29 +0000

    updated: labels on sensors

--- pi_fly/go.py
+++ pi_fly/go.py
@@ -122,9 +122,9 @@ if __name__ == '__main__':
     sdb = SensorsDb(sensors_db)
     sdb.create_db()
 
-    sdb.add_sensor(OneWireTemperatureDecode("28-0015231007ee"))
-    sdb.add_sensor(OneWireTemperatureDecode("28-021571be4cff"))
-    sdb.add_sensor(OneWireTemperatureDecode("28-0517c16726ff")) # loft 2
+    sdb.add_sensor(OneWireTemperatureDecode("28-0015231007ee")) # top of hot water tank
+    sdb.add_sensor(OneWireTemperatureDecode("28-021571be4cff")) # dangling on roof
+    sdb.add_sensor(OneWireTemperatureDecode("28-0517c16726ff")) # solar collector from 2018-11-13, loft before
     sdb.add_sensor(OneWireTemperatureDecode("28-0517c1599eff")) # landing
     sdb.add_sensor(OneWireTemperatureDecode("28-041783906fff")) # bottom of tank
     sdb.run_forever()
