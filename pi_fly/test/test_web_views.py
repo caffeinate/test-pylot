dbf4e577a0668de8a6b606f9ff7397dcc9351fc5
Si Parker <si@plogic.co.uk>
2019-06-09 08:45:56 +0100

    added: actionals' commands can be run from web interface

--- pi_fly/test/test_web_views.py
+++ pi_fly/test/test_web_views.py
@@ -4,10 +4,12 @@ Created on 27 Mar 2019
 @author: si
 '''
 from datetime import datetime
+from multiprocessing import Process
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from pi_fly.actional.actional_management import build_actional_processes, governor_run_forever
 from pi_fly.model import Sensor, Base
 from pi_fly.scoreboard import ScoreBoard
 from pi_fly.web_view import create_app
@@ -19,7 +21,8 @@ class TestWebViews(BaseTest):
     def setUp(self):
         super().setUp()
 
-        self.app = create_app(self.config, ScoreBoard())
+        self.scoreboard = ScoreBoard()
+        self.app = create_app(self.config, self.scoreboard)
         self.test_client = self.app.test_client()
 
         # useful when using DB in memory
@@ -34,9 +37,15 @@ class TestWebViews(BaseTest):
         DBSession = sessionmaker(bind=engine)
         self.db_session = DBSession()
 
+        # used by tests that spin up more of the system, e.g. actionals
+        self.proc_table = []
+
     def tearDown(self):
         super().tearDown()
         self.request_context.pop()
+
+        if self.proc_table:
+            self.shutdown_procs()
 
     def test_empty_db(self):
         rv = self.test_client.get('/')
@@ -74,3 +83,54 @@ class TestWebViews(BaseTest):
         rv = self.test_client.get('/sensor_scoreboard/')
         self.assertEqual(200, rv.status_code)
         self.assertIn(b'1553982125.797958', rv.data, "Time of fake input sensor not found.")
+
+    def run_actionals(self):
+        """
+        run actionals from config and governor to manage them. Store
+        details to allow :method:`shutdown_procs` on self.
+        """
+        actional_details = build_actional_processes(self.config, self.scoreboard)
+        ac_names = []
+        self.proc_table = []
+        for ac_name, ac_parts in actional_details.items():
+            # can't pickle a process so just put the comms part on
+            self.scoreboard.update_value(ac_name, {'comms': ac_parts['comms']})
+            ac_names.append(ac_name)
+            self.proc_table.append(ac_parts['process'])
+            ac_parts['process'].start()
+
+        p = Process(target=governor_run_forever, args=(self.scoreboard, ac_names, None))
+        self.proc_table.append(p)
+        p.start()
+
+    def shutdown_procs(self):
+        for p in self.proc_table:
+            p.terminate()
+
+        for p in self.proc_table:
+            if p.is_alive():
+                p.join()
+
+        self.proc_table = []
+
+    def test_run_command(self):
+        """
+        actionals should make a available a list of commands and it should be possible to
+        send a command to an actional's comms pipe via the scoreboard.
+        """
+        self.run_actionals()
+
+        rv = self.test_client.get('/run_command/')
+        self.assertEqual(200, rv.status_code)
+        # Command from :class:`actional.dummy.DummyActional`
+        self.assertIn(b'Log &#34;hello command ABC&#34;', rv.data)
+
+        # next POST it
+        rv = self.test_client.post('/run_command/',
+                                   data={'actional_name': 'fake_actional_0',
+                                         'command': 'ABC',
+                                        }
+                                   )
+        self.assertEqual(200, rv.status_code)
+        # Command from :class:`actional.dummy.DummyActional`
+        self.assertIn(b'Running....fake_actional_0 .. ABC', rv.data)
--- pi_fly/web_view.py
+++ pi_fly/web_view.py
@@ -5,9 +5,10 @@ Created on 15 Apr 2018
 '''
 from datetime import datetime
 
-from flask import Flask, render_template, current_app
+from flask import Flask, render_template, current_app, abort, request
 from flask_sqlalchemy import SQLAlchemy
 
+from pi_fly.actional.abstract import CommsMessage
 from pi_fly.devices.abstract import AbstractSensor
 from pi_fly.model import Sensor
 
@@ -74,5 +75,34 @@ def create_app(settings_class, scoreboard):
         page_vars = dict(sensors = p)
         return render_template("sensor_scoreboard.html", **page_vars)
 
-    return app
+    @app.route('/run_command/', methods=['GET', 'POST'])
+    def run_actional_command():
+        """
+        GET lists available commands
+        POST Sends a user selected command to an actional
+        """
+        ac_command = {}
+        for ac in current_app.config['ACTIONALS']:
+            ac_command[ac.name] = []
+            for command_template in ac.available_commands:
+                cmd_summary = (command_template.command, command_template.description)
+                ac_command[ac.name].append(cmd_summary)
+        page_vars = {'actionals_with_commands': ac_command}
 
+        if request.method == 'POST':
+            target_actional = request.values.get('actional_name', None)
+            target_command = request.values.get('command', None)
+            if target_actional not in ac_command:
+                abort(400, f"Unknown actional {target_actional}")
+
+            try:
+                actional_comms = scoreboard.get_current_value(target_actional)['comms']
+            except KeyError:
+                abort(500, "Actional not found in the scoreboard")
+
+            actional_comms.send(CommsMessage(action="command", message=target_command))
+            page_vars['message'] = f"Running....{target_actional} .. {target_command}"
+
+        return render_template("run_command.html", **page_vars)
+
+    return app
