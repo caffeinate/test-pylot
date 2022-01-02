31870cf1253ab4af30a908c1f13cd5813fa2e9e4
Si Parker <si@plogic.co.uk>
2022-01-02 09:29:45 +0000

    updated: grid_producer function to a class responsible for making lock and shared memory updated: cell values into a dictionary ready for date-times updated: JS to read app's frames and display each frame for 500ms

--- FlaskSvgGrid/app.py
+++ FlaskSvgGrid/app.py
@@ -7,32 +7,46 @@ import gunicorn.app.base
 
 
 def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
+    # return (multiprocessing.cpu_count() * 2) + 1
+    return 1
 
 
-def grid_producer(access_lock, shared_memory_label, grid_dimension):
-    init_list = []
-    for x in range(grid_dimension):
-        for y in range(grid_dimension):
-            init_list.extend([x, y, "#000"])
+class GridProducer:
+    def __init__(self, grid_dimension):
+        self.grid_dimension = grid_dimension
+        self.access_lock = None
+        self.shared_list = None
 
-    with access_lock:
-        shared_list = shared_memory.ShareableList(init_list, name=shared_memory_label)
+    def initiate(self):
 
-    while True:
-        time.sleep(1)
+        self.access_lock = multiprocessing.Lock()
 
-        # any kind of pattern
-        colours = ["#f00", "#0f0", "#00f", "#ff0"]
-        loop_start = int(time.time())
-        with access_lock:
-            for x in range(grid_dimension):
-                for y in range(grid_dimension):
-                    v = loop_start + (x * grid_dimension) + (x * grid_dimension) * y
-                    colour_choice = colours[v % len(colours)]
-                    grid_position = (x * grid_dimension) + y
-                    # print(shared_list[grid_position], colour_choice, grid_position)
-                    shared_list[(grid_position * 3) + 2] = colour_choice
+        init_list = []
+        for x in range(self.grid_dimension):
+            for y in range(self.grid_dimension):
+                init_list.extend([x, y, "#000"])
+
+        with self.access_lock:
+            self.shared_list = shared_memory.ShareableList(init_list)
+            shared_memory_label = self.shared_list.shm.name
+
+        return self.access_lock, shared_memory_label
+
+    def run(self):
+
+        while True:
+            time.sleep(1)
+
+            # any kind of pattern
+            colours = ["#f00", "#0f0", "#00f", "#ff0"]
+            loop_start = int(time.time())
+            with self.access_lock:
+                for x in range(self.grid_dimension):
+                    for y in range(self.grid_dimension):
+                        v = loop_start + (x * self.grid_dimension) + (x * self.grid_dimension) * y
+                        colour_choice = colours[v % len(colours)]
+                        grid_position = (x * self.grid_dimension) + y
+                        self.shared_list[(grid_position * 3) + 2] = colour_choice
 
 
 class FlaskSharedMemory(Flask):
@@ -40,16 +54,14 @@ class FlaskSharedMemory(Flask):
         super(FlaskSharedMemory, self).__init__(*args, **kwargs)
 
         self.grid_dimension = 10
-        self.shared_memory_label = "shared_memory_z16"
-        self.access_lock = multiprocessing.Lock()
-        # self.shared_list = shared_memory.ShareableList(name=self.shared_memory_label)
+        self.access_lock = self.shared_memory_label = None
 
     def start_sidecar(self):
 
-        time.sleep(2)
-        p = multiprocessing.Process(
-            target=grid_producer, args=(self.access_lock, self.shared_memory_label, self.grid_dimension)
-        )
+        grid_writer = GridProducer(grid_dimension=self.grid_dimension)
+        self.access_lock, self.shared_memory_label = grid_writer.initiate()
+
+        p = multiprocessing.Process(target=grid_writer.run)
         p.start()
 
 
@@ -63,6 +75,10 @@ def root():
 
 @app.route("/frame")
 def frame():
+
+    if current_app.shared_memory_label is None or current_app.access_lock is None:
+        return "Shared memory writer not initialised", 500
+
     shared_list = shared_memory.ShareableList(name=current_app.shared_memory_label)
 
     with current_app.access_lock:
@@ -79,7 +95,13 @@ def frame():
 
             reshaped_grid.append([x, y, colour])
 
-    return jsonify(reshaped_grid)
+    frame_doc = {
+        "cell_values": reshaped_grid,
+        "build_time": None,
+        "valid_until": None,
+    }
+
+    return jsonify(frame_doc)
 
 
 class StandaloneApplication(gunicorn.app.base.BaseApplication):
@@ -105,5 +127,4 @@ if __name__ == "__main__":
         "bind": "%s:%s" % ("127.0.0.1", "8080"),
         "workers": number_of_workers(),
     }
-    # logger.debug("StandaloneApplication running")
     StandaloneApplication(app, options).run()
